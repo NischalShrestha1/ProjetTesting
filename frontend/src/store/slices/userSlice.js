9619c7d18e8e6b4c6d721566b00eeb26ee9d43a2
Sinlahc <nisc6590@gmail.com>
2026-01-04 18:27:07 +0545

    Update API URL to production endpoint

--- frontend/src/store/slices/userSlice.js
+++ frontend/src/store/slices/userSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://animerch-gjcd.onrender.com/api';
 
 // Load user from localStorage
 const loadUserFromStorage = () => {
