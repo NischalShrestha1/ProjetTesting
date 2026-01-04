dfeb9ccbbf1069c96b410d9820e06a5ea6a23ed1
Sinlahc <nisc6590@gmail.com>
2026-01-04 18:26:29 +0545

    Update categoriesSlice.js

--- frontend/src/store/slices/categoriesSlice.js
+++ frontend/src/store/slices/categoriesSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://animerch-gjcd.onrender.com/api';
 
 // Async thunk to fetch categories
 export const fetchCategories = createAsyncThunk(
