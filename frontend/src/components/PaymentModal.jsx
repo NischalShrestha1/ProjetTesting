fa40b12737ec9747451110407c4b2779f473cad8
Sinlahc <nisc6590@gmail.com>
2026-01-04 18:35:25 +0545

    Update PaymentModal.jsx

--- frontend/src/components/PaymentModal.jsx
+++ frontend/src/components/PaymentModal.jsx
@@ -3,7 +3,7 @@ import { useSelector, useAppDispatch } from '../store/hooks'
 import { selectCartItems, selectCartTotal, clearCart, selectUser } from '../store'
 import axios from 'axios'
 
-const backendUrl = "http://localhost:5000/api"
+const backendUrl = "https://animerch-gjcd.onrender.com/api"
 
 export default function PaymentModal({ onClose, user, onOrderSuccess }) {
   const dispatch = useAppDispatch()
