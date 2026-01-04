ad5819dc0d0af4b893c2a1ec0a207e1bf79d14c7
Sinlahc <nisc6590@gmail.com>
2026-01-04 19:23:05 +0545

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ const io = new Server(server, {
 
 // CORS configuration to allow credentials
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175', 'http://localhost:3000'],
+  origin: ['*'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
