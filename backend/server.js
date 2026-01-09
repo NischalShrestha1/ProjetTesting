a2c3820945938c8b28581ae589738bfbc711510f
Sinlahc <nisc6590@gmail.com>
2026-01-09 11:00:09 +0545

    cors updated

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ const io = new Server(server, {
 
 // CORS configuration to allow credentials
 app.use(cors({
-  origin: ['https://animerch-indol.vercel.app'],
+  origin: ['http://localhost:5173','http://localhost:5174','http://localhost:5175','http://localhost:3000','https://animerch-indol.vercel.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
